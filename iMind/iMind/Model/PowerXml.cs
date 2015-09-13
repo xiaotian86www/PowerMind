20cd589c57ae9f04786976a02302e7ad64099109
xiaotian86www <xiaotian86www@163.com>
2015-09-13 22:49:07 +0800

    新增powerXml类继承自xmldocument类，用于丰富模型层的内容，简化接口

---
+++ iMind/iMind/Model/PowerXml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace PowerMind.Model
+{
+    class PowerXml : XmlDocument
+    {
+        private bool IsModify { get; set; }
+
+        private String XmlPath { get; set; }
+
+        public void Save()
+        {
+            if (IsModify && null != XmlPath)
+            {
+                Save(XmlPath);
+            }
+        }
+
+        public override void Save(string filename)
+        {
+            FileInfo fi = new FileInfo(filename);
+            if (fi.Exists && (IsModify || !fi.FullName.Equals(XmlPath)))
+                base.Save(filename);
+        }
+    }
+}
