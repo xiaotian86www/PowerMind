474f79352125e9c91e0a6047f6afa6c6ff2d98c3
xiaotian86www <xiaotian86www@163.com>
2015-09-21 08:34:46 +0800

    Mind辅助类，用于转换数据形式

---
+++ iMind/iMind/Control/MindConvert.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerMind.Control
+{
+    static class MindConvert
+    {
+        public static Rectangle StringToRectangle(String str)
+        {
+            String[] subStr = str.Split(',');
+            Rectangle rectangle = new Rectangle(Convert.ToInt32(subStr[0]), Convert.ToInt32(subStr[1]), Convert.ToInt32(subStr[2]), Convert.ToInt32(subStr[3]));
+            return rectangle;
+        }
+
+        public static String RectangleToString(Rectangle rectangle)
+        {
+            String str = rectangle.X + "," + rectangle.Y + "," + rectangle.Width + "," + rectangle.Height;
+            return str;
+        }
+    }
+}
